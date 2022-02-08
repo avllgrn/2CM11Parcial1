4b13da09f24622359c9a38804db1b82f97750522
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-02-08 16:07:31 -0600

    Suma de dos números con cin y cout

--- main.cpp
+++ main.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main(void){
+    int a, b, c;
 
-    cout << "Hello world!" << endl;
+    cout << "Ingresa un numero ";
+    cin >> a;
+
+    cout << "Ingresa otro numero ";
+    cin >> b;
+
+    c = a+b;
+
+    cout << "a = " << a << endl
+         << "b = " << b << endl
+         << "c = " << c << endl;
 
     return 0;
 }
