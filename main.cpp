f1460a14b33be4ff5510971e9a29d5a8aa1f2a98
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-02-10 08:14:07 -0600

    Suma de dos puntos con instancias de la clase Punto

--- main.cpp
+++ main.cpp
@@ -1,24 +1,86 @@
 #include <iostream>
+#include <math.h>
+#include <fstream>
 using namespace std;
 
-struct Punto{
+class Punto{
+private:
     double x;
     double y;
+public:
+    Punto(void){
+        this->x = 0.0;
+        this->y = 0.0;
+        //cout << "Objeto construido, this -> " << this << endl;
+    };
+    Punto(double x, double y){
+        this->x = x;
+        this->y = y;
+        //cout << "Objeto construido, this -> " << this << endl;
+    };
+    ~Punto(void){
+        //cout << "Objeto destruido, this -> " << this << endl;
+    };
+    void pideleAlUsuarioTusDatos(void){
+        cout<<"Dame mi x ";
+        cin>>this->x;
+        cout<<"Dame mi y ";
+        cin>>this->y;
+    };
+    void muestraTusDatos(void){
+        cout << "(" << this->x << ", " << this->y <<")";
+    };
+    void guardaTusDatos(ofstream& ASalida) {
+        ASalida << "(" << this->x
+				<< ", " << this->y
+				<< ")" << endl;
+    };
+    void cargaTusDatos(ifstream& AEntrada) {
+        char caracter;
+        AEntrada >> caracter;
+        AEntrada >> this->x;
+        AEntrada >> caracter;
+        AEntrada >> this->y;
+        AEntrada >> caracter;
+    };
+    double dameTuX(void){
+        return this->x;
+    };
+    void modificaTuX(double x){
+        this->x = x;
+    };
+    double dameTuY(void){
+        return this->y;
+    };
+    void modificaTuY(double y){
+        this->y = y;
+    };
+    void modificaTusDatos(double x, double y){
+        this->x = x;
+        this->y = y;
+    };
 };
+
 int main(void){
     //Codifica un programa que sume dos puntos en un plano cartesiano
 
-    struct Punto A, B, C;
+    Punto A, B, C;
 
-    cout<<"Dame las coordenadas de A ";
-    cin>>A.x>>A.y;
-    cout<<"Dame las coordenadas de B ";
-    cin>>B.x>>B.y;
+    cout<<endl<<"Dame las coordenadas de A "<<endl;
+    A.pideleAlUsuarioTusDatos();
 
-    C.x = A.x + B.x;
-    C.y = A.y + B.y;
+    cout<<endl<<"Dame las coordenadas de B "<<endl;
+    B.pideleAlUsuarioTusDatos();
 
-    cout<<"("<<C.x<<","<<C.y<<")"<<endl;
+    C.modificaTuX( A.dameTuX() + B.dameTuX() );
+    C.modificaTuY( A.dameTuY() + B.dameTuY() );
+
+    cout<<endl<<"  A";
+    A.muestraTusDatos();
+    cout<<endl<<"+ B";
+    B.muestraTusDatos();
+    cout<<endl<<"= C";
+    C.muestraTusDatos();
 
     return 0;
 }
