bd66b2fdda2bbe2f0f1278aced99757f62b98522
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-02-21 07:41:21 -0600

    Clase Complejo

--- main.cpp
+++ main.cpp
@@ -3,119 +3,71 @@
 #include <fstream>
 using namespace std;
 
-class Punto{
+class Complejo{
 private:
-    double x;
-    double y;
+    double real;
+    double imaginario;
 public:
-    Punto(void){
-        this->x = 0.0;
-        this->y = 0.0;
+    Complejo(void){
+        this->real = 0.0;
+        this->imaginario = 0.0;
         //cout << "Objeto construido, this -> " << this << endl;
     };
-    Punto(double x, double y){
-        this->x = x;
-        this->y = y;
+    Complejo(double real, double imaginario){
+        this->real = real;
+        this->imaginario = imaginario;
         //cout << "Objeto construido, this -> " << this << endl;
     };
-    ~Punto(void){
+    ~Complejo(void){
         //cout << "Objeto destruido, this -> " << this << endl;
     };
     void pideleAlUsuarioTusDatos(void){
-        cout<<"Dame mi x ";
-        cin>>this->x;
-        cout<<"Dame mi y ";
-        cin>>this->y;
+        cout<<"Dame mi real ";
+        cin>>this->real;
+        cout<<"Dame mi imaginario ";
+        cin>>this->imaginario;
     };
     void muestraTusDatos(void){
-        cout << "(" << this->x << ", " << this->y <<")";
+        cout << this->real;
+        if(this->imaginario<0)
+            cout << this->imaginario;
+        else
+            cout << "+" << this->imaginario;
+		cout << "i";
     };
     void guardaTusDatos(ofstream& ASalida) {
-        ASalida << "(" << this->x
-				<< ", " << this->y
-				<< ")" << endl;
+        ASalida << this->real;
+        if(this->imaginario<0)
+            ASalida << this->imaginario;
+        else
+            ASalida << "+" << this->imaginario;
+		ASalida << "i";
     };
     void cargaTusDatos(ifstream& AEntrada) {
         char caracter;
+        AEntrada >> this->real;
         AEntrada >> caracter;
-        AEntrada >> this->x;
-        AEntrada >> caracter;
-        AEntrada >> this->y;
+        AEntrada >> this->imaginario;
         AEntrada >> caracter;
     };
-    double dameTuX(void){
-        return this->x;
+    double dameTuReal(void){
+        return this->real;
     };
-    void modificaTuX(double x){
-        this->x = x;
+    void modificaTuReal(double real){
+        this->real = real;
     };
-    double dameTuY(void){
-        return this->y;
+    double dameTuImaginario(void){
+        return this->imaginario;
     };
-    void modificaTuY(double y){
-        this->y = y;
+    void modificaTuImaginario(double imaginario){
+        this->imaginario = imaginario;
     };
-    void modificaTusDatos(double x, double y){
-        this->x = x;
-        this->y = y;
+    void modificaTusDatos(double real, double imaginario){
+        this->real = real;
+        this->imaginario = imaginario;
     };
 };
 
-//Codifica un programa que calcule el area y el perimetro
-//de un rectangulo representado por dos puntos en un plano
-//cartesiano: P y Q son esquinas opuestas del rectangulo y
-//su base y su altura siempre son paralelas a sus respectivos ejes
-
-float calculaBase(Punto P, Punto Q);
-float calculaAltura(Punto P, Punto Q);
-float calculaArea(Punto P, Punto Q);
-float calculaPerimetro(Punto P, Punto Q);
-
 int main(void){
-    Punto P, Q;
-
-    //1. Pide datos
-    cout<<endl<<"Ingresa las coordenadas de P"<<endl;
-    P.pideleAlUsuarioTusDatos();
-    cout<<endl<<"Ingresa las coordenadas de Q"<<endl;
-    Q.pideleAlUsuarioTusDatos();
-
-    if(P.dameTuX() != Q.dameTuX()
-       &&
-       P.dameTuY() != Q.dameTuY()
-    ){
-        //2. Calcula formula(s)
-        //3. Muestra resultado(s)
-        cout<<endl
-            <<"Base = "<<calculaBase(P,Q)<<endl
-            <<"Altura = "<<calculaAltura(P,Q)<<endl
-            <<"Area = "<<calculaArea(P,Q)<<endl
-            <<"Perimetro = "<<calculaPerimetro(P,Q)<<endl;
-    }
-    else
-        cout<<endl<<"Error! Datos ingresados son de una recta..."<<endl;
-
     return 0;
 }
-
-
-
-float calculaBase(Punto P, Punto Q){
-    return fabs(Q.dameTuX()-P.dameTuX());
-}
-
-float calculaAltura(Punto P, Punto Q){
-    return fabs(Q.dameTuY()-P.dameTuY());
-}
-
-float calculaArea(Punto P, Punto Q){
-    return calculaBase(P,Q) * calculaAltura(P,Q);
-}
-
-float calculaPerimetro(Punto P, Punto Q){
-    return 2*calculaBase(P,Q) + 2*calculaAltura(P,Q);
-}
-
-
-
-
