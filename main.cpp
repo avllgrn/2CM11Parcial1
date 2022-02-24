70f200e9cde05d411218b1195b305fa1df5f8020
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-02-24 07:12:44 -0600

    Operaciones con objetos Complejo, en main

--- main.cpp
+++ main.cpp
@@ -69,5 +69,72 @@ public:
 };
 
 int main(void){
+    Complejo C1, C2, C3, C4, C5, C6;
+
+    cout<<endl<<"Ingresa C1"<<endl;
+    C1.pideleAlUsuarioTusDatos();
+    cout<<endl<<"Ingresa C1"<<endl;
+    C2.pideleAlUsuarioTusDatos();
+
+    C3.modificaTuReal(
+		C1.dameTuReal() + C2.dameTuReal()
+	);
+    C3.modificaTuImaginario(
+		C1.dameTuImaginario() + C2.dameTuImaginario()
+	);
+
+    C4.modificaTuReal(
+		C1.dameTuReal() - C2.dameTuReal()
+	);
+    C4.modificaTuImaginario(
+		C1.dameTuImaginario() - C2.dameTuImaginario()
+	);
+
+    C5.modificaTuReal(
+		C1.dameTuReal()*C2.dameTuReal() - C1.dameTuImaginario()*C2.dameTuImaginario()
+	);
+    C5.modificaTuImaginario(
+		C1.dameTuReal()*C2.dameTuImaginario() + C2.dameTuReal()*C1.dameTuImaginario()
+	);
+
+    C6.modificaTuReal(
+        (C1.dameTuReal()*C2.dameTuReal() + C1.dameTuImaginario()*C2.dameTuImaginario())
+        /
+        (pow(C2.dameTuReal(),2) + pow(C2.dameTuImaginario(),2))
+    );
+    C6.modificaTuImaginario(
+        (C2.dameTuReal()*C1.dameTuImaginario() - C1.dameTuReal()*C2.dameTuImaginario())
+        /
+        (pow(C2.dameTuReal(),2) + pow(C2.dameTuImaginario(),2))
+    );
+
+    cout<<endl<<"( ";
+    C1.muestraTusDatos();
+    cout<<" ) + ( ";
+    C2.muestraTusDatos();
+    cout<<" ) = ";
+    C3.muestraTusDatos();
+
+    cout<<endl<<"( ";
+    C1.muestraTusDatos();
+    cout<<" ) - ( ";
+    C2.muestraTusDatos();
+    cout<<" ) = ";
+    C4.muestraTusDatos();
+
+    cout<<endl<<"( ";
+    C1.muestraTusDatos();
+    cout<<" ) * ( ";
+    C2.muestraTusDatos();
+    cout<<" ) = ";
+    C5.muestraTusDatos();
+
+    cout<<endl<<"( ";
+    C1.muestraTusDatos();
+    cout<<" ) / ( ";
+    C2.muestraTusDatos();
+    cout<<" ) = ";
+    C6.muestraTusDatos();
+
     return 0;
 }
