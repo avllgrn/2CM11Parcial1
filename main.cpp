48e6abc80021caec89a9fbf54ebb182021e2a881
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-03-03 07:21:13 -0600

    Ejercicios de email

--- main.cpp
+++ main.cpp
@@ -2,56 +2,19 @@
 #include <stdlib.h>
 using namespace std;
 
-bool esNumero(char c);
-bool esMayuscula(char c);
-bool esMinuscula(char c);
-bool esLetra(char c);
-bool esCaracterEspecial(char c);
-
 int main(void){
-    //Tarea2: Codifica un programa que lea una cadena
-    //y diga si es plalindrome.
+    //Ejercicio 1:
+    //Codifica un programa que lea una direccion de email.
+    //El programa debe mostrar cada parte de la direccion por separado.
+    //Nota: El usuario ingresa una direccion valida:
+    //Usuario:
+    //Servidor:
+    //Dominio:
 
-    string Cadena,Destino, Origen;
-    int i,n;
-
-    cout<<"Ingresa una cadena ";
-    getline(cin,Cadena);
-
-    n = Cadena.size();
-    for(i=0; i<n; i++)
-        if(!esCaracterEspecial(Cadena.at(i)))
-            Origen.push_back(tolower(Cadena.at(i)));
-
-    n = Origen.size();
-    for(i=n-1; i>=0; i--)
-        Destino.push_back(Origen.at(i));
-
-
-    cout<<"Cadena:  "<<Cadena<<endl;
-    cout<<"Origen:  "<<Origen<<endl;
-    cout<<"Destino: "<<Destino<<endl<<endl;
-
-    if(Origen.compare(Destino)==0)
-        cout<<Cadena<<" es palindrome"<<endl<<endl;
-    else
-        cout<<Cadena<<" NO es palindrome"<<endl<<endl;
+    //Ejercicio 2:
+    //Despues de solucionar el Ejercicio 1,
+    //a) sustitye el nombre del servidor por ipn
+    //b) sustitye el nombre del dominio por mx
 
     return 0;
-}
-
-bool esNumero(char c){
-    return 48<=c && c<=57;
-}
-bool esMayuscula(char c){
-    return 65<=c && c<=90;
-}
-bool esMinuscula(char c){
-    return 97<=c && c<=122;
-}
-bool esLetra(char c){
-    return esMayuscula(c) || esMinuscula(c);
-}
-bool esCaracterEspecial(char c){
-    return !esNumero(c) && !esLetra(c);
 }
