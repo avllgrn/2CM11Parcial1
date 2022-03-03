1f91f2f761d85df024a029265a60b05f45502042
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-03-03 08:29:40 -0600

    Solucion a Ejercicio 1 de email

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void){
-    //Ejercicio 1:
+    //Ejercicio 1: (Duracion de 15 mins, lo explicamos a las 7:43)
     //Codifica un programa que lea una direccion de email.
     //El programa debe mostrar cada parte de la direccion por separado.
     //Nota: El usuario ingresa una direccion valida:
@@ -11,10 +11,19 @@ int main(void){
     //Servidor:
     //Dominio:
 
-    //Ejercicio 2:
-    //Despues de solucionar el Ejercicio 1,
-    //a) sustitye el nombre del servidor por ipn
-    //b) sustitye el nombre del dominio por mx
+    string email, usuario, servidor, dominio;
+    cout<<"Ingresa email ";cin>>email;
+
+    usuario.append(email, 0, email.find('@'));
+    servidor.append(email, email.find('@')+1, email.find('.')-email.find('@')-1);
+    dominio.append(email, email.find('.'), email.size()-email.find('.'));
+
+    cout<<endl
+        <<"Tu email es: "<<email<<endl
+        <<"Tu usuario es : "<<usuario<<endl
+        <<"Tu servidor es : "<<servidor<<endl
+        <<"Tu dominio es : "<<dominio<<endl
+        <<endl;
 
     return 0;
 }
