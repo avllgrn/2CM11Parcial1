2c0626ba0ad2770a33a7934885cd51df2a03d6ca
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-03-04 08:22:29 -0600

    Clase Natural

--- main.cpp
+++ main.cpp
@@ -1,40 +1,401 @@
 #include <iostream>
 #include <stdlib.h>
+#include <sstream>
+#include <fstream>
 using namespace std;
 
+class Natural{
+private:
+    int iValor;
+    string sValor;
+    void verificaSValor(void){
+        stringstream ss;
+        this->iValor = abs(atoi(this->sValor.c_str()));
+        ss << this->iValor;
+        ss >> this->sValor;
+    };
+public:
+    Natural(void){
+        this->iValor = 0;
+        this->sValor = "0";
+        //cout << "Objeto construido, this -> " << this << endl;
+    };
+    Natural(int iValor){
+        stringstream ss;
+        this->iValor = abs(iValor);
+        ss << this->iValor;
+        ss >> this->sValor;
+        //cout << "Objeto construido, this -> " << this << endl;
+    };
+    Natural(string sValor){
+        this->sValor = sValor;
+        this->verificaSValor();
+        //cout << "Objeto construido, this -> " << this << endl;
+    };
+    Natural(char cValor){
+        stringstream ss;
+        if(cValor<48||cValor>57){
+            iValor = (int) cValor;
+            ss << this->iValor;
+            ss >> this->sValor;
+        }
+        else{
+            ss << cValor;
+            ss >> this->sValor;
+            iValor = atoi(sValor.c_str());
+        }
+        //cout << "Objeto construido, this -> " << this << endl;
+    };
+    ~Natural(void){
+        //cout << "Objeto destruido, this -> " << this << endl;
+    };
+    void pideleAlUsuarioTusDatos(void){
+        cout<<"Dame mi iValor ";
+        cin>>this->sValor;
+        this->verificaSValor();
+    };
+    void muestraTusDatos(void){
+        cout << this->iValor;
+    };
+    void guardaTusDatos(ofstream& ASalida) {
+        ASalida << this->iValor;
+    };
+    void cargaTusDatos(ifstream& AEntrada) {
+        AEntrada >> sValor;
+        this->verificaSValor();
+    };
+    int dameTuIValor(void){
+        return this->iValor;
+    };
+    void modificaTuIValor(int iValor){
+        stringstream ss;
+        this->iValor = abs(iValor);
+        ss << this->iValor;
+        ss >> this->sValor;
+    };
+    void modificaTuIValor(char cValor){
+        stringstream ss;
+        if(cValor<48||cValor>57){
+            iValor = (int) cValor;
+            ss << this->iValor;
+            ss >> this->sValor;
+        }
+        else{
+            ss << cValor;
+            ss >> this->sValor;
+            iValor = atoi(sValor.c_str());
+        }
+        //cout << "Objeto construido, this -> " << this << endl;
+    };
+    string dameTuSValor(void){
+        return this->sValor;
+    }
+    void modificaTuSValor(string sValor){
+        this->sValor = sValor;
+        this->verificaSValor();
+    };
+};
+
 int main(void){
-    //Ejercicio 1: (Duracion de 15 mins, lo explicamos a las 7:43)
-    //Codifica un programa que lea una direccion de email.
-    //El programa debe mostrar cada parte de la direccion por separado.
-    //Nota: El usuario ingresa una direccion valida:
-    //Usuario:
-    //Servidor:
-    //Dominio:
+    Natural A;
+    int iA;
+    string sA;
 
-    string email, usuario, servidor, dominio;
-    cout<<"Ingresa email ";cin>>email;
+    iA = A.dameTuIValor();
+    sA = A.dameTuSValor();
 
-    usuario.append(email, 0, email.find('@'));
-    servidor.append(email, email.find('@')+1, email.find('.')-email.find('@')-1);
-    dominio.append(email, email.find('.'), email.size()-email.find('.'));
+    cout<<" A = ";
+    A.muestraTusDatos();
+    cout<<endl;
+    cout<<"iA = "<<iA<<endl
+        <<"sA = "<<sA<<endl<<endl;
+    system("pause");
+    system("cls");
 
-    cout<<endl
-        <<"Tu email es: "<<email<<endl
-        <<"Tu usuario es : "<<usuario<<endl
-        <<"Tu servidor es : "<<servidor<<endl
-        <<"Tu dominio es : "<<dominio<<endl
-        <<endl;
+    Natural B(1);
+    int iB;
+    string sB;
 
-    //Ejercicio 2:
-    //Despues de solucionar el Ejercicio 1,
-    //a) sustitye el nombre del servidor por ipn
-    //b) sustitye el nombre del dominio por mx
+    iB = B.dameTuIValor();
+    sB = B.dameTuSValor();
 
-    email.replace(email.find('@')+1, email.find('.')-email.find('@')-1, "ipn");
-    email.replace(email.find('.')+1, email.size()-email.find('.')-1, "mx");
+    cout<<" B = ";
+    B.muestraTusDatos();
+    cout<<endl;
+    cout<<"iB = "<<iB<<endl
+        <<"sB = "<<sB<<endl<<endl;
+    system("pause");
+    system("cls");
 
-    cout<<endl
-        <<"Tu email es: "<<email<<endl<<endl;
+    Natural C(-2);
+    int iC;
+    string sC;
+
+    iC = C.dameTuIValor();
+    sC = C.dameTuSValor();
+
+    cout<<" C = ";
+    C.muestraTusDatos();
+    cout<<endl;
+    cout<<"iC = "<<iC<<endl
+        <<"sC = "<<sC<<endl<<endl;
+    system("pause");
+    system("cls");
+
+    Natural D("3");
+    int iD;
+    string sD;
+
+    iD = D.dameTuIValor();
+    sD = D.dameTuSValor();
+
+    cout<<" D = ";
+    D.muestraTusDatos();
+    cout<<endl;
+    cout<<"iD = "<<iD<<endl
+        <<"sD = "<<sD<<endl<<endl;
+    system("pause");
+    system("cls");
+
+    Natural E("-4");
+    int iE;
+    string sE;
+
+    iE = E.dameTuIValor();
+    sE = E.dameTuSValor();
+
+    cout<<" E = ";
+    E.muestraTusDatos();
+    cout<<endl;
+    cout<<"iE = "<<iE<<endl
+        <<"sE = "<<sE<<endl<<endl;
+    system("pause");
+    system("cls");
+
+    Natural F("CINCO");
+    int iF;
+    string sF;
+
+    iF = F.dameTuIValor();
+    sF = F.dameTuSValor();
+
+    cout<<" F = ";
+    F.muestraTusDatos();
+    cout<<endl;
+    cout<<"iF = "<<iF<<endl
+        <<"sF = "<<sF<<endl<<endl;
+    system("pause");
+    system("cls");
+
+    Natural G('6');
+    int iG;
+    string sG;
+
+    iG = G.dameTuIValor();
+    sG = G.dameTuSValor();
+
+    cout<<" G = ";
+    G.muestraTusDatos();
+    cout<<endl;
+    cout<<"iG = "<<iG<<endl
+        <<"sG = "<<sG<<endl<<endl;
+    system("pause");
+    system("cls");
+
+    Natural H('@');
+    int iH;
+    string sH;
+
+    iH = H.dameTuIValor();
+    sH = H.dameTuSValor();
+
+    cout<<" H = ";
+    H.muestraTusDatos();
+    cout<<endl;
+    cout<<"iH = "<<iH<<endl
+        <<"sH = "<<sH<<endl<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<" A = ";
+    A.muestraTusDatos();
+    cout<<endl;
+    cout<<"iA = "<<iA<<endl
+        <<"sA = "<<sA<<endl<<endl;
+
+    A.pideleAlUsuarioTusDatos();
+    iA = A.dameTuIValor();
+    sA = A.dameTuSValor();
+
+    cout<<" A = ";
+    A.muestraTusDatos();
+    cout<<endl;
+    cout<<"iA = "<<iA<<endl
+        <<"sA = "<<sA<<endl<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<" B = ";
+    B.muestraTusDatos();
+    cout<<endl;
+    cout<<"iB = "<<iB<<endl
+        <<"sB = "<<sB<<endl<<endl;
+
+    B.modificaTuIValor(8);
+    iB = B.dameTuIValor();
+    sB = B.dameTuSValor();
+
+    cout<<" B = ";
+    B.muestraTusDatos();
+    cout<<endl;
+    cout<<"iB = "<<iB<<endl
+        <<"sB = "<<sB<<endl<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<" C = ";
+    C.muestraTusDatos();
+    cout<<endl;
+    cout<<"iC = "<<iC<<endl
+        <<"sC = "<<sC<<endl<<endl;
+
+    C.modificaTuIValor('9');
+    iC = C.dameTuIValor();
+    sC = C.dameTuSValor();
+
+    cout<<" C = ";
+    C.muestraTusDatos();
+    cout<<endl;
+    cout<<"iC = "<<iC<<endl
+        <<"sC = "<<sC<<endl<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<" D = ";
+    D.muestraTusDatos();
+    cout<<endl;
+    cout<<"iD = "<<iD<<endl
+        <<"sD = "<<sD<<endl<<endl;
+
+    D.modificaTuIValor('@');
+    iD = D.dameTuIValor();
+    sD = D.dameTuSValor();
+
+    cout<<" D = ";
+    D.muestraTusDatos();
+    cout<<endl;
+    cout<<"iD = "<<iD<<endl
+        <<"sD = "<<sD<<endl<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<" E = ";
+    E.muestraTusDatos();
+    cout<<endl;
+    cout<<"iE = "<<iE<<endl
+        <<"sE = "<<sE<<endl<<endl;
+
+    E.modificaTuSValor("6");
+    iE = E.dameTuIValor();
+    sE = E.dameTuSValor();
+
+    cout<<" E = ";
+    E.muestraTusDatos();
+    cout<<endl;
+    cout<<"iE = "<<iE<<endl
+        <<"sE = "<<sE<<endl<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<" F = ";
+    F.muestraTusDatos();
+    cout<<endl;
+    cout<<"iF = "<<iF<<endl
+        <<"sF = "<<sF<<endl<<endl;
+
+    F.modificaTuSValor("66");
+    iF = F.dameTuIValor();
+    sF = F.dameTuSValor();
+
+    cout<<" F = ";
+    F.muestraTusDatos();
+    cout<<endl;
+    cout<<"iF = "<<iF<<endl
+        <<"sF = "<<sF<<endl<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<" G = ";
+    G.muestraTusDatos();
+    cout<<endl;
+    cout<<"iG = "<<iG<<endl
+        <<"sG = "<<sG<<endl<<endl;
+
+    G.modificaTuSValor("sesenta y siete");
+    iG = G.dameTuIValor();
+    sG = G.dameTuSValor();
+
+    cout<<" G = ";
+    G.muestraTusDatos();
+    cout<<endl;
+    cout<<"iG = "<<iG<<endl
+        <<"sG = "<<sG<<endl<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<" H = ";
+    H.muestraTusDatos();
+    cout<<endl;
+    cout<<"iH = "<<iH<<endl
+        <<"sH = "<<sH<<endl<<endl;
+    cout<<" A = ";
+    A.muestraTusDatos();
+    cout<<endl;
+    cout<<"iA = "<<iA<<endl
+        <<"sA = "<<sA<<endl<<endl;
+    cout<<" B = ";
+    B.muestraTusDatos();
+    cout<<endl;
+    cout<<"iB = "<<iB<<endl
+        <<"sB = "<<sB<<endl<<endl;
+
+    H.modificaTuIValor(A.dameTuIValor()+B.dameTuIValor());
+    iH = H.dameTuIValor();
+    sH = H.dameTuSValor();
+
+    cout<<" H = A+B = ";
+    H.muestraTusDatos();
+    cout<<endl;
+    cout<<"iH = "<<iH<<endl
+        <<"sH = "<<sH<<endl<<endl;
+    system("pause");
+    system("cls");
+
+    cout<<" E = ";
+    E.muestraTusDatos();
+    cout<<endl;
+    cout<<"iE = "<<iE<<endl
+        <<"sE = "<<sE<<endl<<endl;
+    cout<<" C = ";
+    C.muestraTusDatos();
+    cout<<endl;
+    cout<<"iC = "<<iC<<endl
+        <<"sC = "<<sC<<endl<<endl;
+    cout<<" D = ";
+    D.muestraTusDatos();
+    cout<<endl;
+    cout<<"iD = "<<iD<<endl
+        <<"sD = "<<sD<<endl<<endl;
+
+    E.modificaTuSValor(C.dameTuSValor()+D.dameTuSValor());
+    iE = E.dameTuIValor();
+    sE = E.dameTuSValor();
+
+    cout<<" E = C+D = ";
+    E.muestraTusDatos();
+    cout<<endl;
+    cout<<"iE = "<<iE<<endl
+        <<"sE = "<<sE<<endl<<endl;
 
     return 0;
 }
