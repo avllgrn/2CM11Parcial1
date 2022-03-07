0f3b3a663d02380dc033ae11935d8f226cc8f876
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-03-07 07:38:39 -0600

    Operaciones aritmeticas con objetos tipo Natura

--- main.cpp
+++ main.cpp
@@ -96,306 +96,62 @@ public:
     };
 };
 
+
+
+
 int main(void){
-    Natural A;
-    int iA;
-    string sA;
+    Natural A,B,C,E,D,F,G;
 
-    iA = A.dameTuIValor();
-    sA = A.dameTuSValor();
-
-    cout<<" A = ";
-    A.muestraTusDatos();
-    cout<<endl;
-    cout<<"iA = "<<iA<<endl
-        <<"sA = "<<sA<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    Natural B(1);
-    int iB;
-    string sB;
-
-    iB = B.dameTuIValor();
-    sB = B.dameTuSValor();
-
-    cout<<" B = ";
-    B.muestraTusDatos();
-    cout<<endl;
-    cout<<"iB = "<<iB<<endl
-        <<"sB = "<<sB<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    Natural C(-2);
-    int iC;
-    string sC;
-
-    iC = C.dameTuIValor();
-    sC = C.dameTuSValor();
-
-    cout<<" C = ";
-    C.muestraTusDatos();
-    cout<<endl;
-    cout<<"iC = "<<iC<<endl
-        <<"sC = "<<sC<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    Natural D("3");
-    int iD;
-    string sD;
-
-    iD = D.dameTuIValor();
-    sD = D.dameTuSValor();
-
-    cout<<" D = ";
-    D.muestraTusDatos();
-    cout<<endl;
-    cout<<"iD = "<<iD<<endl
-        <<"sD = "<<sD<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    Natural E("-4");
-    int iE;
-    string sE;
-
-    iE = E.dameTuIValor();
-    sE = E.dameTuSValor();
-
-    cout<<" E = ";
-    E.muestraTusDatos();
-    cout<<endl;
-    cout<<"iE = "<<iE<<endl
-        <<"sE = "<<sE<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    Natural F("CINCO");
-    int iF;
-    string sF;
-
-    iF = F.dameTuIValor();
-    sF = F.dameTuSValor();
-
-    cout<<" F = ";
-    F.muestraTusDatos();
-    cout<<endl;
-    cout<<"iF = "<<iF<<endl
-        <<"sF = "<<sF<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    Natural G('6');
-    int iG;
-    string sG;
-
-    iG = G.dameTuIValor();
-    sG = G.dameTuSValor();
-
-    cout<<" G = ";
-    G.muestraTusDatos();
-    cout<<endl;
-    cout<<"iG = "<<iG<<endl
-        <<"sG = "<<sG<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    Natural H('@');
-    int iH;
-    string sH;
-
-    iH = H.dameTuIValor();
-    sH = H.dameTuSValor();
-
-    cout<<" H = ";
-    H.muestraTusDatos();
-    cout<<endl;
-    cout<<"iH = "<<iH<<endl
-        <<"sH = "<<sH<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    cout<<" A = ";
-    A.muestraTusDatos();
-    cout<<endl;
-    cout<<"iA = "<<iA<<endl
-        <<"sA = "<<sA<<endl<<endl;
-
+    cout<<"A"<<endl;
     A.pideleAlUsuarioTusDatos();
-    iA = A.dameTuIValor();
-    sA = A.dameTuSValor();
+    cout<<"B"<<endl;
+    B.pideleAlUsuarioTusDatos();
 
-    cout<<" A = ";
+    C=A.dameTuIValor()+B.dameTuIValor();
+    D=A.dameTuIValor()-B.dameTuIValor();
+    E=A.dameTuIValor()*B.dameTuIValor();
+    if(B.dameTuIValor()!=0){
+        F=A.dameTuIValor()/B.dameTuIValor();
+        G=A.dameTuIValor()%B.dameTuIValor();
+    }
+
+    cout<<endl;
+    cout<<"  A = ";
     A.muestraTusDatos();
     cout<<endl;
-    cout<<"iA = "<<iA<<endl
-        <<"sA = "<<sA<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    cout<<" B = ";
+    cout<<"  B = ";
     B.muestraTusDatos();
     cout<<endl;
-    cout<<"iB = "<<iB<<endl
-        <<"sB = "<<sB<<endl<<endl;
-
-    B.modificaTuIValor(8);
-    iB = B.dameTuIValor();
-    sB = B.dameTuSValor();
-
-    cout<<" B = ";
-    B.muestraTusDatos();
-    cout<<endl;
-    cout<<"iB = "<<iB<<endl
-        <<"sB = "<<sB<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    cout<<" C = ";
+    cout<<"A+B = ";
     C.muestraTusDatos();
     cout<<endl;
-    cout<<"iC = "<<iC<<endl
-        <<"sC = "<<sC<<endl<<endl;
-
-    C.modificaTuIValor('9');
-    iC = C.dameTuIValor();
-    sC = C.dameTuSValor();
-
-    cout<<" C = ";
-    C.muestraTusDatos();
-    cout<<endl;
-    cout<<"iC = "<<iC<<endl
-        <<"sC = "<<sC<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    cout<<" D = ";
+    cout<<"A-B = ";
     D.muestraTusDatos();
     cout<<endl;
-    cout<<"iD = "<<iD<<endl
-        <<"sD = "<<sD<<endl<<endl;
-
-    D.modificaTuIValor('@');
-    iD = D.dameTuIValor();
-    sD = D.dameTuSValor();
-
-    cout<<" D = ";
-    D.muestraTusDatos();
-    cout<<endl;
-    cout<<"iD = "<<iD<<endl
-        <<"sD = "<<sD<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    cout<<" E = ";
+    cout<<"A*B = ";
     E.muestraTusDatos();
     cout<<endl;
-    cout<<"iE = "<<iE<<endl
-        <<"sE = "<<sE<<endl<<endl;
-
-    E.modificaTuSValor("6");
-    iE = E.dameTuIValor();
-    sE = E.dameTuSValor();
-
-    cout<<" E = ";
-    E.muestraTusDatos();
-    cout<<endl;
-    cout<<"iE = "<<iE<<endl
-        <<"sE = "<<sE<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    cout<<" F = ";
-    F.muestraTusDatos();
-    cout<<endl;
-    cout<<"iF = "<<iF<<endl
-        <<"sF = "<<sF<<endl<<endl;
-
-    F.modificaTuSValor("66");
-    iF = F.dameTuIValor();
-    sF = F.dameTuSValor();
-
-    cout<<" F = ";
-    F.muestraTusDatos();
-    cout<<endl;
-    cout<<"iF = "<<iF<<endl
-        <<"sF = "<<sF<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    cout<<" G = ";
-    G.muestraTusDatos();
-    cout<<endl;
-    cout<<"iG = "<<iG<<endl
-        <<"sG = "<<sG<<endl<<endl;
-
-    G.modificaTuSValor("sesenta y siete");
-    iG = G.dameTuIValor();
-    sG = G.dameTuSValor();
-
-    cout<<" G = ";
-    G.muestraTusDatos();
-    cout<<endl;
-    cout<<"iG = "<<iG<<endl
-        <<"sG = "<<sG<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    cout<<" H = ";
-    H.muestraTusDatos();
-    cout<<endl;
-    cout<<"iH = "<<iH<<endl
-        <<"sH = "<<sH<<endl<<endl;
-    cout<<" A = ";
-    A.muestraTusDatos();
-    cout<<endl;
-    cout<<"iA = "<<iA<<endl
-        <<"sA = "<<sA<<endl<<endl;
-    cout<<" B = ";
-    B.muestraTusDatos();
-    cout<<endl;
-    cout<<"iB = "<<iB<<endl
-        <<"sB = "<<sB<<endl<<endl;
-
-    H.modificaTuIValor(A.dameTuIValor()+B.dameTuIValor());
-    iH = H.dameTuIValor();
-    sH = H.dameTuSValor();
-
-    cout<<" H = A+B = ";
-    H.muestraTusDatos();
-    cout<<endl;
-    cout<<"iH = "<<iH<<endl
-        <<"sH = "<<sH<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    cout<<" E = ";
-    E.muestraTusDatos();
-    cout<<endl;
-    cout<<"iE = "<<iE<<endl
-        <<"sE = "<<sE<<endl<<endl;
-    cout<<" C = ";
-    C.muestraTusDatos();
-    cout<<endl;
-    cout<<"iC = "<<iC<<endl
-        <<"sC = "<<sC<<endl<<endl;
-    cout<<" D = ";
-    D.muestraTusDatos();
-    cout<<endl;
-    cout<<"iD = "<<iD<<endl
-        <<"sD = "<<sD<<endl<<endl;
-
-    E.modificaTuSValor(C.dameTuSValor()+D.dameTuSValor());
-    iE = E.dameTuIValor();
-    sE = E.dameTuSValor();
-
-    cout<<" E = C+D = ";
-    E.muestraTusDatos();
-    cout<<endl;
-    cout<<"iE = "<<iE<<endl
-        <<"sE = "<<sE<<endl<<endl;
+    if(B.dameTuIValor()!=0){
+        cout<<"A/B = ";
+        F.muestraTusDatos();
+        cout<<endl;
+        cout<<"A%B = ";
+        G.muestraTusDatos();
+        cout<<endl;
+    }
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
